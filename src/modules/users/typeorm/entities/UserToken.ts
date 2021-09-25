bd8df932cc85178b99adcb9e489581c86330700a
RafaelRossales <rapha2704@gmail.com>
2021-09-24 21:28:56 -0300

    implementing forgot password

---
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn, Generated } from "typeorm";
+
+
+/**
+ * @ignore
+ */
+@Entity('users')
+class UserToken {
+  @PrimaryGeneratedColumn('uuid')
+  id:string;
+
+  @Column()
+  @Generated('uuid')
+  token:string;
+
+  @Column()
+  user_id:string;
+
+  @CreateDateColumn()
+  created_at:Date;
+
+  @UpdateDateColumn()
+  updated_at:Date;
+}
+
+export default UserToken;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,6 +6,7 @@ import { errors } from 'celebrate';
 import routes from './routes'
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm/';
+import uploadConfig from '@config/upload'
 
 
 
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files',express.static(uploadConfig.directory));
 app.use(routes);
 app.use(errors()); // Celebrate
 
