e18716db83e83313dab0153d1f32e020a7f8c8d2
RafaelRossales <rapha2704@gmail.com>
2021-09-24 21:40:41 -0300

    implementing SendForgotPasswordEmailService

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,40 @@
+'use strict'
+
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm"
+import UsersRepository from "../typeorm/respositories/UsersRepository";
+import UserTokenRepository from "../typeorm/respositories/UserTokensRepository";
+
+
+/**
+ * @description - Dados informados pelo usuário
+ * @interface
+ */
+interface IRequest{
+  email:string,
+}
+
+/**
+ * @description - Serviço de criação de produtos
+ */
+class SendForgotPasswordEmailService{
+
+  public async execute({ email }:IRequest):Promise<void>{
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UserTokenRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if(!user) throw new AppError('User does not exists');
+
+
+    //Se usuário exitir, o token será gerado
+    const token = await userTokenRepository.generateToken(user.id);
+
+    console.log(token)
+
+  }
+}
+
+
+export default SendForgotPasswordEmailService;
